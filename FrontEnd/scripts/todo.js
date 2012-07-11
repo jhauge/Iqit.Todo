e88c43d7fc7a76a02f49a24e2811e567a5fd892b
Jesper Hauge <jesper@iqit.dk>
2012-07-11 20:58:58 +0200

    Created HTML setup
    Imported HTML5 boilerplate and created basic HTML + CSS for application

---
+++ FrontEnd/scripts/todo.js
@@ -0,0 +1,15 @@
+﻿// Namespace --------------------------------------------------------
+var iqit = iqit || {};
+
+// Todo class -------------------------------------------------------
+iqit.Todo = function (id) {
+    this.id = id;
+    this.title = "";
+    this.comment = "";
+}
+
+iqit.Todo.prototype = {
+    toString: function () {
+        return this.id + " : " + this.title;
+    }
+}
