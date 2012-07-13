62178ce09119ece3620a4edaf853e3586dba54b6
Jesper Hauge <jesper@iqit.dk>
2012-07-14 00:02:50 +0200

    Added jquery ui
    Added jquery ui, and implemented "Add item" button as jquery ui button
    using a knockout bindinghandler

---
+++ FrontEnd/scripts/models/ko.bindings.js
@@ -0,0 +1,12 @@
+﻿/// <reference path="../libs/knockout-2.1.0.debug.js" />
+
+ko.bindingHandlers.jqButton = {
+    init: function (element) {
+        $(element).button();
+    },
+
+    update: function (element, valueAccessor) {
+        var currentValue = valueAccessor();
+        $(element).button("option", "disabled", currentValue.enable === false);
+    }
+};
